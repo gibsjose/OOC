fc05a9f27242ff0e3671034cbb14925313892bcb
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-09-03 22:36:46 -0400

    Initial commit

---
+++ src/ooc.c
@@ -0,0 +1,21 @@
+/**********************************************************
+*   OOC
+*
+*   03 Sep 2015
+*
+*   Joe Gibson (gibsjose@mail.gvsu.edu)
+*
+*   License: MIT (https://user.mit-license.org)
+*
+*   https://www.gibsjose.com
+*   https://www.gibsjose.com
+**********************************************************/
+
+#include <stdio.h>
+#include "ooc.h"
+
+int main(int argc, char * argv[]) {
+    printf("Rocket!\n");
+
+    return 0;
+}
---
+++ src/ooc.h
@@ -0,0 +1,17 @@
+/**********************************************************
+*   OOC
+*
+*   03 Sep 2015
+*
+*   Joe Gibson (gibsjose@mail.gvsu.edu)
+*
+*   License: MIT (https://user.mit-license.org)
+*
+*   https://www.gibsjose.com
+*   https://www.gibsjose.com
+**********************************************************/
+
+#ifndef OOC_H
+#define OOC_H
+
+#endif//OOC_H
